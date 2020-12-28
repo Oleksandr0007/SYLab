1cf2d8cfca5ff1e8ae324d9c82cc6081ca7b2899
Oleksandr0007 <72250197+Oleksandr0007@users.noreply.github.com>
2020-12-28 15:46:09 +0200

    Add files via upload

---
+++ Lab3.1/Lab3.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
+
+
+namespace Lab3._1
+{
+    public class Rectangle
+    {
+        private double side1 { get; set; }
+        private double side2 { get; set; }
+        public double getside1()
+        {
+            Console.WriteLine("Please enter the first side: ");
+            side1 = double.Parse(Console.ReadLine());
+            return side1;
+        }
+        public double getside2()
+        {
+            Console.WriteLine("Please enter the second side: ");
+            side2 = double.Parse(Console.ReadLine());
+            return side2;
+        }
+        public double getper()
+        {
+            double perimeter;
+            perimeter = (getside1() + getside2()) * 2;
+            return perimeter;
+        }
+        public double getpl()
+        {
+            double square;
+            square = getside1() * getside2();
+            return square;
+        }
+
+    }
+
+    class Prog1
+    {
+        static void Main(string[] args)
+        {
+            Rectangle perimeter = new Rectangle();
+            Rectangle square = new Rectangle();
+            Console.WriteLine("Perimetr is: " + perimeter.getper());
+            Console.WriteLine("Square is: " + square.getpl());
+        }
+
+    }
+
+}
